f4e444335c0a4505b965a9a2f2f9f8834d673237
harumaki1220 <haruma181220@gmail.com>
2026-04-19 04:49:13 +0900

    feat: 石を置いてひっくり返すロジックを実装

---
+++ src/logic/board.ts
@@ -0,0 +1,67 @@
+import type { Board } from "../types/othello";
+
+const DIRECTIONS = [
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+];
+
+export const getOpponent = (color: "BLACK" | "WHITE"): "BLACK" | "WHITE" => {
+  return color === "BLACK" ? "WHITE" : "BLACK";
+};
+
+export const putDisc = (
+  board: Board,
+  color: "BLACK" | "WHITE",
+  x: number,
+  y: number,
+): Board | null => {
+  if (board[y][x] !== "EMPTY") return null;
+
+  const newBoard = structuredClone(board);
+  let canFlip = false;
+
+  for (const [dy, dx] of DIRECTIONS) {
+    const opponentColor = getOpponent(color);
+    let step = 1;
+
+    while (
+      y + dy * step >= 0 &&
+      y + dy * step <= 7 &&
+      x + dx * step >= 0 &&
+      x + dx * step <= 7 &&
+      board[y + dy * step][x + dx * step] === opponentColor
+    ) {
+      step += 1;
+    }
+
+    const nx = x + dx * step;
+    const ny = y + dy * step;
+
+    if (
+      ny >= 0 &&
+      ny <= 7 &&
+      nx >= 0 &&
+      nx <= 7 &&
+      board[ny][nx] === color &&
+      step > 1
+    ) {
+      canFlip = true;
+
+      for (let i = 1; i < step; i++) {
+        newBoard[y + dy * i][x + dx * i] = color;
+      }
+    }
+  }
+
+  if (!canFlip) return null;
+
+  newBoard[y][x] = color;
+
+  return newBoard;
+};
